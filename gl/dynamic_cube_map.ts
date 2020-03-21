4e287f994a2ce2cf5db9ca605e82257dad3850c7
Tom Madams <tom.ej.madams@gmail.com>
2020-03-21 12:47:39 -0700

    add multiple render target support to DynamicCubeMap

--- gl/dynamic_cube_map.ts
+++ gl/dynamic_cube_map.ts
@@ -3,8 +3,9 @@ import * as vec3 from 'toybox/math/vec3';
 
 import {GL} from 'toybox/gl/constants';
 import {Context} from 'toybox/gl/context';
+import {TextureInternalFormat, TextureMinFilter} from 'toybox/gl/constants';
 import {Framebuffer} from 'toybox/gl/framebuffer';
-import {Texture2D, TextureCube, TextureCubeDef} from 'toybox/gl/texture';
+import {Texture2D, TextureCube} from 'toybox/gl/texture';
 import {TypedArray, TypedArrayConstructor} from 'toybox/util/array';
 
 let tmp = vec3.newZero();
@@ -45,34 +46,32 @@ export class DynamicCubeMap {
   // Per-face data.
   faces: DynamicCubeMap.Face[] = [];
 
-  // Color cube map.
-  color: TextureCube;
+  // Color textures.
+  color: TextureCube[] = [];
 
   // 2D depth texture shared between all faces.
   depth: Texture2D;
 
-  // Resolution of each face.
-  size: number;
-
   /**
     * @param ctx
-    * @param options
-    * @param near near clip plane distance
-    * @param far far clip plane distance
+    * @param size cube map resolution..
+    * @param format array of framebuffer formats, one per render target.
+    * @param near near clip plane distance.
+    * @param far far clip plane distance.
     * @param readPixels if true, allocate arrays for each face for the pixel
     *                   data to be read back into.
     */
-  constructor(ctx: Context, options: TextureCubeDef,
+  constructor(ctx: Context, public size: number, format: DynamicCubeMap.Format[],
               public near: number, public far: number, public readPixels=false) {
-    this.size = options.size;
-
     // TODO(tom): add support for depth-only cube maps.
     // Create a cube texture.
-    this.color = ctx.newTextureCube(options);
+    for (let f of format) {
+      this.color.push(ctx.newTextureCube({size: size, filter: f.filter, format: f.format}));
+    }
 
     // Create a 2D depth texture that will be shared between faces.
     // TODO(tom): support depth-only, no-depth, and different depth precisions.
-    this.depth = ctx.newTexture2D({size: options.size, format: GL.DEPTH_COMPONENT16});
+    this.depth = ctx.newTexture2D({size: size, format: GL.DEPTH_COMPONENT16});
 
     let eyePos = vec3.setZero(tmp);
     this.proj = mat4.newPerspective(0.5 * Math.PI, 1, near, far);
@@ -83,48 +82,50 @@ export class DynamicCubeMap {
     }
 
     if (this.readPixels) {
-      let numChannels: number;
-      switch (this.color.format) {
-        case GL.RGBA:
-        case GL.RGBA_INTEGER:
-          numChannels = 4;
-          break;
+      for (let target of this.color) {
+        let numChannels: number;
+        switch (target.format) {
+          case GL.RGBA:
+          case GL.RGBA_INTEGER:
+            numChannels = 4;
+            break;
 
-        case GL.RGB:
-        case GL.RGB_INTEGER:
-          numChannels = 3;
-          break;
+          case GL.RGB:
+          case GL.RGB_INTEGER:
+            numChannels = 3;
+            break;
 
-        case GL.RED:
-        case GL.RED_INTEGER:
-          numChannels = 1;
-          break;
+          case GL.RED:
+          case GL.RED_INTEGER:
+            numChannels = 1;
+            break;
 
-        default:
-          throw new Error(`unsupported format ${this.color.format}`);
-      }
+          default:
+            throw new Error(`unsupported format ${target.format}`);
+        }
 
-      let ctor: TypedArrayConstructor;
-      switch (this.color.type) {
-        case GL.UNSIGNED_BYTE:
-          ctor = Uint8Array;
-          break;
+        let ctor: TypedArrayConstructor;
+        switch (target.type) {
+          case GL.UNSIGNED_BYTE:
+            ctor = Uint8Array;
+            break;
 
-        case GL.UNSIGNED_SHORT_5_6_5:
-        case GL.UNSIGNED_SHORT_4_4_4_4:
-        case GL.UNSIGNED_SHORT_5_5_5_1:
-          ctor = Uint16Array;
-          break;
+          case GL.UNSIGNED_SHORT_5_6_5:
+          case GL.UNSIGNED_SHORT_4_4_4_4:
+          case GL.UNSIGNED_SHORT_5_5_5_1:
+            ctor = Uint16Array;
+            break;
 
-        case GL.FLOAT:
-          ctor = Float32Array;
-          break;
+          case GL.FLOAT:
+            ctor = Float32Array;
+            break;
 
-        default:
-          throw new Error(`unsupported type ${this.color.type}`);
-      }
-      for (let face of this.faces) {
-        face.pixels = new ctor(this.size * this.size * numChannels);
+          default:
+            throw new Error(`unsupported type ${target.type}`);
+        }
+        for (let [i, face] of this.faces.entries()) {
+          face.pixels.push(new ctor(this.size * this.size * numChannels));
+        }
       }
     }
   }
@@ -140,9 +141,19 @@ export class DynamicCubeMap {
 }
 
 export namespace DynamicCubeMap {
+  export interface Format {
+    filter?: TextureMinFilter;
+    format: TextureInternalFormat;
+  }
+
   export class Face {
     viewProj = mat4.newZero();
-    pixels: TypedArray = null;
+
+    // If the parent cube map has requested readPixels support, pixels contains
+    // TypedArrays of correct size and format for writing the data two: one for
+    // each render target.
+    // Empty otherwise.
+    pixels: TypedArray[] = [];
 
     constructor(public name: string, public fb: Framebuffer, public view: mat4.Type, proj: mat4.Type) {
       mat4.mul(this.viewProj, proj, this.view);
